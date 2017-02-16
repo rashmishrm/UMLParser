8d241dc416bc3b6a55e65fb8b698a80724a73d78
rashmishrm <rashmishrm74@gmail.com>
2017-02-16 15:59:43 -0800

    Sample code for JavaParser usage

---
+++ UMLGenerator/src/main/java/com/sjsu/project/tryout/TestClass.java
@@ -0,0 +1,23 @@
+package com.sjsu.project.tryout;
+
+public class TestClass {
+
+    public String myVariable;
+
+    private String myPrivVariable;
+
+    public void myMethod() {
+    }
+
+    private void myMethod1(Sample a) {
+    }
+
+    public String getMyPrivVariable() {
+	return myPrivVariable;
+    }
+
+    public String getMyVariable() {
+	return myVariable;
+    }
+
+}
