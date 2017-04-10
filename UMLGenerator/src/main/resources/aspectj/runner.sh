8c06938eba42fe6d0750298c9e4c61042f8fc2a8
rashmishrm <rashmishrm74@gmail.com>
2017-04-10 12:42:35 -0700

    Weaving aspect at compile time

--- UMLGenerator/src/main/resources/aspectj/runner.sh
+++ UMLGenerator/src/main/resources/aspectj/runner.sh
@@ -6,6 +6,7 @@ export ASPECTJ_RT=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-pro
 export ASPECT_LIB=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/aspectj1.8/lib
 export PATH=$PATH:$ASPECTJ_HOME/bin:$JAVA_HOME/bin
 #CLASSPATH=$CLASSPATH:$ASPECTJ_HOME:$ASPECTJ_RT:$ASPECT_LIB
+PROJECT_HOME=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/UMLGenerator
 
 INPUT_FOLDER=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test
 
@@ -21,15 +22,14 @@ CLASSES_DIR=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/r
 rm -rf $CLASSES_DIR
 mkdir $CLASSES_DIR
 
-cp /Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/UMLGenerator/src/main/java/com/sjsu/umlgenerator/aspect/MethodInterceptorjAJ $INPUT_FOLDER
+cp $PROJECT_HOME/src/main/java/com/sjsu/umlgenerator/aspect/MethodInterceptor.aj $INPUT_FOLDER
 
 # Compile the sources
 echo "Compiling..."
-javac -classpath $CLASSPATH -g -d $CLASSES_DIR $INPUT_FOLDER/*.java
+#javac -classpath $CLASSPATH -g -d $CLASSES_DIR $INPUT_FOLDER/*.java
 
-#echo "Weaving aspect..."
-#java -cp $CLASSPATH org.aspectj.tools.ajc.Main -source 1.5 -inpath $CLASSES_DIR -aspectpath ./src/main/java -outjar $JAR_DIR/test.jar
+java -cp $CLASSPATH org.aspectj.tools.ajc.Main -source 1.5 -d $CLASSES_DIR $INPUT_FOLDER/*.java $INPUT_FOLDER/MethodInterceptor.aj
 
 # Run the example and check that aspect logic is applied
-#echo "Running the sample..."
-#java -cp $CLASSPATH:$JAR_DIR/test.jar com.aspectj.Main
+echo "Running the sample..."
+java -cp $CLASSPATH:$CLASSES_DIR Main
