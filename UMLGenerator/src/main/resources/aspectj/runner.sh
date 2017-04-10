1cbdf1f67f403747c088bbb90e26a1a6bd7401b9
rashmishrm <rashmishrm74@gmail.com>
2017-04-10 12:21:26 -0700

    Compiling all classes

--- UMLGenerator/src/main/resources/aspectj/runner.sh
+++ UMLGenerator/src/main/resources/aspectj/runner.sh
@@ -3,17 +3,33 @@
 export CLASSPATH=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/aspectj1.8/lib/aspectjrt.jar:.
 export ASPECTJ_HOME=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/aspectj1.8
 export ASPECTJ_RT=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/aspectj1.8/lib/aspectjrt.jar
+export ASPECT_LIB=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/aspectj1.8/lib
 export PATH=$PATH:$ASPECTJ_HOME/bin:$JAVA_HOME/bin
+#CLASSPATH=$CLASSPATH:$ASPECTJ_HOME:$ASPECTJ_RT:$ASPECT_LIB
 
+INPUT_FOLDER=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test
 
+for i in 'aspectjtools.jar' 'aspectjrt.jar'
+do
+    CLASSPATH=$CLASSPATH:/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/aspectj1.8/lib/$i
+done
+
+JAR_DIR=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test/compile-time
+
+CLASSES_DIR=/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test/compile-time
+
+rm -rf $CLASSES_DIR
+mkdir $CLASSES_DIR
+
+cp /Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/UMLGenerator/src/main/java/com/sjsu/umlgenerator/aspect/MethodInterceptorjAJ $INPUT_FOLDER
 
 # Compile the sources
 echo "Compiling..."
-javac -classpath $CLASSPATH -g -d /Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test /Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test MethodInterceptorjAJ.java
+javac -classpath $CLASSPATH -g -d $CLASSES_DIR $INPUT_FOLDER/*.java
 
-echo "Weaving aspect..."
-java -cp $CLASSPATH org.aspectj.tools.ajc.Main -source 1.5 -inpath /Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test -aspectpath ./src/main/java -outjar /Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test/test.jar
+#echo "Weaving aspect..."
+#java -cp $CLASSPATH org.aspectj.tools.ajc.Main -source 1.5 -inpath $CLASSES_DIR -aspectpath ./src/main/java -outjar $JAR_DIR/test.jar
 
 # Run the example and check that aspect logic is applied
-echo "Running the sample..."
-java -cp $CLASSPATH:/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test/test.jar com.aspectj.Main
+#echo "Running the sample..."
+#java -cp $CLASSPATH:$JAR_DIR/test.jar com.aspectj.Main
