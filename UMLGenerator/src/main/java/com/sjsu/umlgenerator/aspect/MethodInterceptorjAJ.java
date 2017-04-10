2bbb9536d0c1195ffe8c9318d813bc8756454d92
rashmishrm <rashmishrm74@gmail.com>
2017-04-10 11:27:12 -0700

    Tryouts: Method interceptor

--- UMLGenerator/src/main/java/com/sjsu/umlgenerator/aspect/MethodInterceptorjAJ.java
+++ UMLGenerator/src/main/java/com/sjsu/umlgenerator/aspect/MethodInterceptorjAJ.java
@@ -1,24 +1,23 @@
 package com.sjsu.umlgenerator.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class MethodInterceptorjAJ {
 
 
-    @Pointcut("execution(* HelloWorld.sayHello(..))")
+    @Pointcut("(execution(* *(..))&& !cflow(within(MethodInterceptorjAJ)))")
     public void logging() {
     }
 
-    @Around("logging()")
+    @Before("logging()")
     public Object logging(ProceedingJoinPoint thisJoinPoint) throws Throwable {
 	System.out.println("Before " + thisJoinPoint.getSignature());
 	final Object ret = thisJoinPoint.proceed();
 	System.out.println("After " + thisJoinPoint.getSignature());
-
 	return ret;
     }
 
