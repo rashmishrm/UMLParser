56530744b9375a26257127f785149bd0b45eeb24
rashmishrm <rashmishrm74@gmail.com>
2017-04-10 11:27:31 -0700

    Tryouts: Runner

---
+++ UMLGenerator/src/main/java/com/sjsu/umlgenerator/Runner.java
@@ -0,0 +1,43 @@
+package com.sjsu.umlgenerator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+    private String executeCommand(String command) {
+
+	final StringBuffer output = new StringBuffer();
+
+	Process p;
+	try {
+	    p = Runtime.getRuntime().exec(command);
+	    p.waitFor();
+	    final BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+	    String line = "";
+	    while ((line = reader.readLine()) != null) {
+		output.append(line + "\n");
+	    }
+
+	} catch (final Exception e) {
+	    e.printStackTrace();
+	}
+
+	return output.toString();
+
+    }
+
+    public void run(String folder) {
+	folder = "/Users/rashmisharma/Documents/Spring_2017/cmpe202/personal-project/rashmi/test-cases/umlparser/uml-sequence-test";
+	final String command = "ajc -outxml -outjar aspects.jar  MethodInterceptorjAJ.java";
+	executeCommand(command);
+
+    }
+
+    public static void main(String[] args) {
+	final Runner runner = new Runner();
+	runner.run("");
+    }
+
+}
