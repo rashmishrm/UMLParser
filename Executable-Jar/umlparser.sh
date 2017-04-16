2a250d0788aa0c4545a2de9561456fed600bfe0c
rashmishrm <rashmishrm74@gmail.com>
2017-04-15 20:05:33 -0700

    Umlparser shell script

---
+++ Executable-Jar/umlparser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+INPUT_FOLDER=$1
+OUPUT_FILE_NAME=$2
+
+OPTION=1
+
+if [ $INPUT_FOLDER = "sequence" ]
+then
+OPTION=2
+fi
+
+# Compile the sources
+echo "Creating diagram"
+
+java -jar umlgenerator.jar -d $INPUT_FOLDER -f $OUPUT_FILE_NAME -t $OPTION
